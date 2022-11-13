072e3548e3ecac17d73c237f154b03eb4680d711
ValeLattanzi <vale.lattanzi15@gmial.com>
2022-11-13 17:28:21 -0300

    Modificaciones de la vista

--- viewEstadia.py
+++ viewEstadia.py
@@ -9,24 +9,19 @@ from Estadia import registrarEstadia
 from Estadia import calcularPrecioEstadia
 # Crea la ventana
 ventana = Tk()
-ventana.geometry("600x400")
+ventana.geometry("610x400")
 ventana.title("Estadia")
-ventana.maxsize(width = 600,height=400)
-ventana.config(background="light blue")
-
+ventana.maxsize(width = 610,height=400)
+ventana.config(background="light grey")
+# Genera el entry de la fecha actual la carga
 entryFechaActual = Entry(ventana)
 entryFechaActual.place(x = 20, y = 5)
 entryFechaActual.insert(0, datetime.now().strftime("%d/%m/%Y")) #Ingreso la fecha en el entry
 entryFechaActual.config(state = "disabled")
 
-# GroupBox Habitacion
+# Genera el grupo de los datos correspondientes a la habitacion
+# region GroupBox Habitacion
 frHabitacion = ttk.Labelframe(ventana, text="Habitacion")
-style = ttk.Style()
-style.configure(
-    "TLabelFrame",
-    foreground = "red",
-    padding = [5,5,5,5]
-)
 frHabitacion.config(width=585, height=80)
 frHabitacion.place(x=10, y=28)
 
@@ -57,48 +52,66 @@ entryPrecio.place(x = 455,y = 0)
 entryPrecio.config(state="disabled")
 
 entryPension.bind("<<ComboboxSelected>>", pensionSeleccionada)
+# endregion
 
-# GroupBox Cliente
-lblNombreYApellido = Label(ventana, text = "Nombre y Apellido:", background="light blue").place(x = 20, y = 120 )
-entryNombreYApellido = Entry(ventana)
-entryNombreYApellido.place(x = 135, y = 120)
+# Crea el grupo de los datos que son referidos al cliente
+# region GroupBox Cliente
+frCliente = Labelframe(ventana, text="Cliente")
+frCliente.config(width=585, height= 200)
+frCliente.place(x = 10, y = 115 )
 
-lblTipoDocumento = Label(ventana, text = "Tipo de Documento:", background="light blue").place(x = 15, y = 150)
-entryTipoDocumento = Combobox(ventana,  state = "readonly")
-entryTipoDocumento.place(x = 135, y = 150)
+lblNombreYApellido = Label(frCliente, text = "Nombre y Apellido:").place(x = 20, y = 0 )
+entryNombreYApellido = Entry(frCliente)
+entryNombreYApellido.place(x = 135, y = 0)
+
+lblTipoDocumento = Label(frCliente, text = "Tipo de Documento:").place(x = 15, y = 25)
+entryTipoDocumento = Combobox(frCliente,  state = "readonly")
+entryTipoDocumento.place(x = 135, y = 25)
 entryTipoDocumento['values'] = ('DNI', 'Pasaporte', 'Libreta Civica', 'Libreta Enrolamiento')
 
-lblNumeroDocumento = Label(ventana, text="Nro Documento:", background="light blue").place(x = 280, y = 150)
-entryNumeroDocumento = Entry(ventana)
-entryNumeroDocumento.place(x = 380,y = 150)
+lblNumeroDocumento = Label(frCliente, text="Nro Documento:").place(x = 280, y = 25)
+entryNumeroDocumento = Entry(frCliente)
+entryNumeroDocumento.place(x = 380,y = 25)
 
-lblPais = Label(ventana, text="Pais Origen:", background="light blue").place(x = 60,y = 180)
-entryPais = Entry(ventana)
-entryPais.place(x = 135, y = 180)
+lblPais = Label(frCliente, text="Pais Origen:").place(x = 60,y = 50)
+entryPais = Entry(frCliente)
+entryPais.place(x = 135, y = 50)
+lblMail = Label(frCliente, text = "Mail:").place(x = 345, y = 50)
+entryMail = Entry(frCliente)
+entryMail.place(x = 380, y = 50)
 
-lblMail = Label(ventana, text = "Mail:", background="light blue").place(x = 345, y = 180)
-entryMail = Entry(ventana)
-entryMail.place(x = 380, y = 180)
+lblPoseeVehiculo = Label(frCliente, text = "Posee Vehículo:").place(x = 40, y = 80)
+entryPoseeVehiculo = Combobox(frCliente, values = ["SI", "NO"],  state = "readonly")
+entryPoseeVehiculo.place(x = 135, y = 80)
+# endregion
 
-lblPoseeVehiculo = Label(ventana, text = "Posee Vehículo:", background="light blue").place(x = 40, y = 210)
-entryPoseeVehiculo = Combobox(ventana, values = ["SI", "NO"],  state = "readonly")
-entryPoseeVehiculo.place(x = 135, y = 210)
-# GroupBox Vehiculo
-lblMarca = Label(ventana, text = "Marca:", background="light blue").place(x = 20 ,y = 280)
-entryMarca = Combobox(ventana,  state = "readonly")
-entryMarca.place(x = 80, y = 280)
+# Crea el grupo de los datos correspondientes al vehiculo
+# region GroupBox Vehiculo
+frVehiculo = Labelframe(frCliente, text="Vehiculo")
+frVehiculo.config(width=565, height=60)
+frVehiculo.place(x = 10, y = 110)
+
+lblMarca = Label(frVehiculo, text = "Marca:").place(x = 20 ,y = 10)
+entryMarca = Combobox(frVehiculo,  state = "readonly")
+entryMarca.place(x = 80, y = 10)
 entryMarca['value'] = ('Abarth', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Cadillac', 'Caterham', 'Chevrolet', 'Citroen', 'Dacia', 'Ferrari', 'Fiat', 'Ford', 'Honda', 'Infiniti', 'Isuzu', 'Iveco', 'Jaguar', 'Jeep', 'Kia', 'KTM', 'Lada', 'Lamborghini', 'Lancia', 'Land Rover', 'Lexus', 'Lotus', 'Maserati', 'Mazda', 'Mercedes-Benz', 'Mini', 'Mitsubishi', 'Morgan', 'Nissan', 'Opel', 'Peugeot', 'Piaggio', 'Porsche', 'Renault', 'Rolls-Royce', 'Seat', 'Skoda', 'Smart', 'SsangYong', 'Subaru', 'Suzuki', 'Tata', 'Tesla', 'Toyota', 'Volkswagen', 'Volv')
 
-lblPatente = Label(ventana,text = "Patente:", background="light blue").place(x = 240, y = 280)
-entryPatente = tk.Entry(ventana)
-entryPatente.place(x = 290, y = 280)
-# GroupBox Botones
+lblPatente = Label(frVehiculo, text = "Patente:").place(x = 240, y = 10)
+entryPatente = tk.Entry(frVehiculo)
+entryPatente.place(x = 300, y = 10)
+# endregion
 
-btnRegistrar = tk.Button(ventana, text = "REGISTRAR", bg="green", command = lambda: registrarEstadia(entryFechaActual.get(), entryHabitacion.get(), entryPension.get(), entryFechaLimite.get(), entryAcompañantes.get(), entryPrecio.get(), entryNumeroDocumento.get(), entryNombreYApellido.get(), entryTipoDocumento.get(), entryPais.get(), entryMail.get(), entryPoseeVehiculo.get(), entryPatente.get(), entryMarca.get()))
-btnRegistrar.place(x = 100, y = 340)
-btnCancelar =  tk.Button(ventana, text = "CANCELAR", bg="red", command = lambda: exit())
-btnCancelar.place(x = 220, y = 340)
+# Crea el grupo de los botones para las acciones permitidas para la pantalla
+# region GroupBox Botones
+frButtons = Labelframe(ventana)
+frButtons.config(width=585, height=70)
+frButtons.place(x = 10, y = 320)
 
+btnRegistrar = tk.Button(frButtons, text = "REGISTRAR", bg="light green", command = lambda: registrarEstadia(entryFechaActual.get(), entryHabitacion.get(), entryPension.get(), entryFechaLimite.get(), entryAcompañantes.get(), entryPrecio.get(), entryNumeroDocumento.get(), entryNombreYApellido.get(), entryTipoDocumento.get(), entryPais.get(), entryMail.get(), entryPoseeVehiculo.get(), entryPatente.get(), entryMarca.get()))
+btnRegistrar.place(x = 50, y = 10)
+btnCancelar =  tk.Button(frButtons, text = "CANCELAR", bg="crimson", command = lambda: ventana.destroy())
+btnCancelar.place(x = 450, y = 10)
+# endregion
 # separador = Separator(ventana, orient='horizontal')
 
 ventana.mainloop()
